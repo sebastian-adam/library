4296eb70676b04134f16fa11b77c556ba4733def
Maldon Meehan and Sebastian Prokuski <maldon@>
2016-07-13 09:52:17 -0700

    add passing tests for is the same book if it has the same attributes and for lets you save the Book object to the database

--- lib/book.rb
+++ lib/book.rb
@@ -1,25 +1,35 @@
 class Book
-  attr_reader(:id, :title, :author, :genre)
+  attr_reader(:id, :title, :author_first, :author_last, :genre)
 
-
-  define_method(:initalize) do |attributes|
+  define_method(:initialize) do |attributes|
     @id = attributes.fetch(:id)
     @title = attributes.fetch(:title)
-    @autor = attributes.fetch(:author)
+    @author_first = attributes.fetch(:author_first)
+    @author_last = attributes.fetch(:author_last)
     @genre = attributes.fetch(:genre)
   end
 
   define_singleton_method(:all) do
-    returned_books = DB.exec('SELECT * FROM books ORDER BY author ASC;')
+    returned_books = DB.exec("SELECT * FROM books ORDER BY author_last ASC;")
     books = []
     returned_books.each() do |book|
       id = book.fetch('id').to_i
       title = book.fetch('title')
-      author = book.fetch('author')
+      author_first = book.fetch('author_first')
+      author_last = book.fetch('author_last')
       genre = book.fetch('genre')
-      books.push(Book.new({:id => id, :title => title, :author => author, :genre => genre}))
+      books.push(Book.new({:id => id, :title => title, :author_first => author_first, :author_last => author_last, :genre => genre}))
     end
     books
   end
+
+  define_method(:==) do |another_book|
+    self.id().==(another_book.id()).&(self.title().==(another_book.title())).&(self.author_first().==(another_book.author_first())).&(self.author_last().==(another_book.author_last())).&(self.genre().==(another_book.genre()))
+  end
+  define_method(:save) do
+    result = DB.exec("INSERT INTO books (title, author_first, author_last, genre) VALUES ('#{@title}', '#{@author_first}', '#{@author_last}', '#{@genre}') RETURNING id;")
+    @id = result.first().fetch('id').to_i()
+  end
+
 
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -6,4 +6,60 @@ describe(Book) do
       expect(Book.all()).to(eq([]))
     end
   end
+
+  describe('#title') do
+    it('returns the title of a given book') do
+      book1 = Book.new({:id => nil, :title => 'Alice in Wonderland', :author_first => 'Lewis', :author_last => 'Carroll', :genre => 'fantasy'})
+      expect(book1.title()).to(eq('Alice in Wonderland'))
+    end
+  end
+
+  describe('#author_first') do
+    it('returns the author_first of a given book') do
+      book1 = Book.new({:id => nil, :title => 'Alice in Wonderland', :author_first => 'Lewis', :author_last => 'Carroll', :genre => 'fantasy'})
+      expect(book1.author_first()).to(eq('Lewis'))
+    end
+  end
+
+  describe('#author_last') do
+    it('returns the author_last of a given book') do
+      book1 = Book.new({:id => nil, :title => 'Alice in Wonderland', :author_first => 'Lewis', :author_last => 'Carroll', :genre => 'fantasy'})
+      expect(book1.author_last()).to(eq('Carroll'))
+    end
+  end
+
+  describe('#genre') do
+    it('returns the genre of a given book') do
+      book1 = Book.new({:id => nil, :title => 'Alice in Wonderland', :author_first => 'Lewis', :author_last => 'Carroll', :genre => 'fantasy'})
+      expect(book1.genre()).to(eq('fantasy'))
+    end
+  end
+
+  describe('#==') do
+    it('is the same book if it has the same attributes') do
+      book1 = Book.new({:id => nil, :title => 'Alice in Wonderland', :author_first => 'Lewis', :author_last => 'Carroll', :genre => 'fantasy'})
+      book2 = Book.new({:id => nil, :title => 'Alice in Wonderland', :author_first => 'Lewis', :author_last => 'Carroll', :genre => 'fantasy'})
+      expect(book1).to(eq(book2))
+    end
+  end
+
+  describe('#save') do
+    it('lets you save the Book object to the database') do
+      book1 = Book.new({:id => nil, :title => 'Alice in Wonderland', :author_first => 'Lewis', :author_last => 'Carroll', :genre => 'fantasy'})
+      book1.save()
+      expect(Book.all()).to(eq([book1]))
+    end
+  end
+
+
+
+  # describe('#id') do
+  #   it('returns the id of a given book') do
+  #     book1 = Book.new({:id => nil, :title => 'Alice in Wonderland', :author_first => 'Lewis', :author_last => 'Carroll', :genre => 'fantasy'})
+  #     expect(book1.id()).to(be_an_instance_of(Fixnum))
+  #   end
+  # end
+
+
+
 end
